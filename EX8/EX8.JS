ea3c53c1e188138f4e6df542a08b70c4a63df70e
bruna126lopes <brunasilvalopes629@gmail.com>
2024-11-04 00:31:19 -0300

    Add files via upload

---
+++ EX8/EX8.JS
@@ -0,0 +1,6 @@
+var x = parseFloat(prompt("Quanto voce ganha por hora?: "));
+var y = parseFloat(prompt("Quantas horas no mes?: "));
+
+res = (x * y) * 30 
+
+console.log("Seu salario e igual: R$", res);
